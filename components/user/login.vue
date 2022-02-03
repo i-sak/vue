e69025145e00ad97e3381c9790a213b401265ed3
kimisak <isak7263@gmail.com>
2022-02-04 01:35:57 +0900

    login

--- components/user/login.vue
+++ components/user/login.vue
@@ -1,3 +1,33 @@
 <template>
-    <h1>login</h1>
+
+    <div>
+        <div v-if="is_login_fail == true">
+            <h3>로그인 실패</h3>
+        </div>
+        <!-- vue.js에선 form tag는 사용하지 않음 -->
+        <h1>login</h1>
+        <input type="text" v-model="user_Id"><br>
+        <input type="password" v-model="user_pw"><br>
+        <button type="button" @click="check_input">로그인</button>
+
+    </div>
 </template>
+<script>
+module.exports = {
+    data : function () {
+        return {
+            is_login_fail : false,
+            // v-model로 data와 연결 시킴
+            user_id : '',
+            user_pw : ''
+        }
+    },
+    methods : {
+        check_input : function() {
+
+        }
+
+    }
+
+}
+</script>
